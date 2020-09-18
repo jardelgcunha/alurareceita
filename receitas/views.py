35b9187d160599c06aed076ffdaa6b96def8a5e6
Jardel <developerprojectfullstack@gmail.com>
2020-09-17 21:50:33 -0300

    update the def receita for access receitas

--- receitas/views.py
+++ receitas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Receita
 
 
@@ -11,5 +11,10 @@ def index(request):
     return render(request, 'index.html', dados)
 
 
-def receita(request):
-    return render(request, 'receita.html')
+def receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+
+    receita_exibir = {
+        'receita': receita
+    }
+    return render(request, 'receita.html', receita_exibir)
