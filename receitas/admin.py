9893e0256a6e576acf66df77a5fa6854790ed58c
Jardel <developerprojectfullstack@gmail.com>
2020-09-21 19:23:34 -0300

    add functionalities in the admin

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Receita
 
-admin.site.register(Receita)
+
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita', 'categoria')
+    list_display_links = ('id', 'nome_receita')
+    search_fields = ('nome_receita',)
+    list_filter = ('categoria',)
+    list_per_page = 2
+
+
+admin.site.register(Receita, ListandoReceitas)
