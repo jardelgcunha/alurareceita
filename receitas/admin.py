819fac11c0a3c89981bb900fa526418b1f83212d
Jardel <developerprojectfullstack@gmail.com>
2020-09-22 19:41:18 -0300

    sort, edit and filter with admin

--- receitas/admin.py
+++ receitas/admin.py
@@ -3,11 +3,12 @@ from .models import Receita
 
 
 class ListandoReceitas(admin.ModelAdmin):
-    list_display = ('id', 'nome_receita', 'categoria')
-    list_display_links = ('id', 'nome_receita')
+    list_display = ('id', 'nome_receita', 'categoria', 'pessoa', 'publicada',)
+    list_display_links = ('id', 'nome_receita',)
     search_fields = ('nome_receita',)
-    list_filter = ('categoria',)
-    list_per_page = 2
+    list_filter = ('categoria', 'publicada',)
+    list_editable = ('publicada',)
+    list_per_page = 10
 
 
 admin.site.register(Receita, ListandoReceitas)
