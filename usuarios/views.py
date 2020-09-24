aa149ac97fe408f75e6408d17b0821028673dcb5
Jardel <developerprojectfullstack@gmail.com>
2020-09-24 15:36:52 -0300

    return render in 'cadastro' and 'login'

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 
 
 def cadastro(request):
-    pass
+    return render(request, 'usuarios/cadastro.html')
 
 
 def login(request):
-    pass
+    return render(request, 'usuarios/login.html')
 
 
 def logout(request):
