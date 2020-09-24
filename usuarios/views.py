faeb883fffe6a0def3107de01b426e0c966aa6c0
Jardel <developerprojectfullstack@gmail.com>
2020-09-24 19:25:46 -0300

    creating user with authentication and validation

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
 
 
 def cadastro(request):
     if request.method == 'POST':
         nome = request.POST['nome']
         email = request.POST['email']
-        password = request.POST['password']
-        password2 = request.POST['password2']
-        print(nome, email, password, password2)
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+        if not nome.strip():
+            print('O campo nome não pode ficar em branco')
+            return redirect('cadastro')
+        if not email.strip():
+            print('O campo email não pode ficar em branco')
+            return redirect('cadastro')
+        if senha != senha2:
+            print('As senhas não são iguais')
+            return redirect('cadastro')
+        if User.objects.filter(email=email).exists():
+            print('Usuário já cadastrado')
+            return redirect('cadastro')
+        user = User.objects.create_user(
+            username=nome, email=email, password=senha)
+        user.save()
+        print('Usuário cadastrado com sucesso')
         return redirect('login')
     else:
         return render(request, 'usuarios/cadastro.html')
