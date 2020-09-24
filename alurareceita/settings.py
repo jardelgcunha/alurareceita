0942e07429f30e54e7261c2c0289a4217ca98da0
Jardel <developerprojectfullstack@gmail.com>
2020-09-23 22:03:54 -0300

    register app 'usuarios' in settings

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'pessoas',
     'receitas',
+    'usuarios',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
